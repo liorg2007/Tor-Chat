68ece85d7d69b36ac1f6c1016362e2a9dc0e2426
Lior G <liorgleizer1@gmail.com>
2024-09-18 13:37:48 +0300

    tested net package

--- test/client.go
+++ test/client.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+var addr string = "localhost"
 
 func main() {
-	fmt.Println("Hello world")
+	var port string
+	fmt.Println("Hello world, Enter the port: ")
+	fmt.Scan(&port)
+
+	fullAddr := net.JoinHostPort(addr, port)
+
+	conn, err := net.Dial("tcp", fullAddr)
+
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected!!!")
 }
