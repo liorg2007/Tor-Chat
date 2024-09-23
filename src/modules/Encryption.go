1deda5bfe087ba448e1272bb5eb999eb8162393b
Lior Gleizer <100088104+liorg2007@users.noreply.github.com>
2024-09-23 08:54:23 +0300

    Key generation for rsa and aes

--- src/modules/Encryption.go
+++ src/modules/Encryption.go
@@ -1,18 +1,46 @@
 package Encryption
 
-import "crypto/rsa"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+)
 
 type Encrypor interface {
 	Encrypt(text []byte) ([]byte, error)
 	Decrypt(ciphertext []byte) ([]byte, error)
-	GenerateKey() error
+	GenerateKey(size int) error
 }
 
 type RSAEncryptor struct {
 	PrivateKey *rsa.PrivateKey
 	PublicKey  *rsa.PublicKey
+	size       int
 }
 
 type AESEncryptor struct {
-	Key []byte
+	Key  []byte
+	size int
+}
+
+func (r *RSAEncryptor) GenerateKey(size int) error {
+	privKey, err := rsa.GenerateKey(rand.Reader, size)
+
+	if err != nil {
+		log.Panic(err)
+		return err
+	}
+
+	r.PrivateKey = privKey
+	r.PublicKey = &privKey.PublicKey
+	r.size = size
+
+	return nil
+}
+
+func (a *AESEncryptor) GenerateKey(size int) error {
+	a.Key = make([]byte, size)
+	a.size = size
+
+	return nil
 }
