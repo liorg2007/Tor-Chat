c423a38dabe9df58d84366eef99733f2bc5edafd
Lior Gleizer <100088104+liorg2007@users.noreply.github.com>
2024-10-08 08:43:43 +0300

    change protocol

--- app/pkg/torpacket/dataType.go
+++ app/pkg/torpacket/dataType.go
@@ -1,37 +1,18 @@
 package torpacket
 
-type RawMessage struct {
-	Code     int
-	JsonData string
+type GetAesRequest struct {
+	RsaKey string
 }
 
-type GetAesMsg struct {
+type GetAesResponse struct {
 	AesKey string
 }
 
-type RedirectMsg struct {
+type RedirectRequest struct {
 	Addr              string
 	RedirectedMessage string
 }
 
-type ReceiveMsg struct {
+type ReceiveRequest struct {
 	Message string
 }
-
-type AckMsg struct {
-	Message string
-}
-
-type ParsedMessage struct {
-	Code int         // The message type
-	Data interface{} // The actual data, which will vary based on the message type
-}
-
-// Message type codes used in the Tor-like protocol.
-const (
-	GetAES   = 100 // Request an AES key from the node.
-	Redirect = 101 // Redirect an encrypted message to another node.
-	Receive  = 102 // Receive and process a message from another node.
-	Destroy  = 103 // Destroy the circuit or session.
-	Ack      = 104 // Acknowledge the reception of a message.
-)
