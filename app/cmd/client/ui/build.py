a8d669ea2aefc16d665f8e9622b651c2d739eaba
Lior Gleizer <100088104+liorg2007@users.noreply.github.com>
2024-12-30 09:30:09 +0200

    builder script for python and instructions for go

---
+++ app/cmd/client/ui/build.py
@@ -0,0 +1,21 @@
+import os
+import PyInstaller.__main__
+
+# Paths
+project_name = "my_customtkinter_app"  # Name of the executable
+entry_file = "main.py"  # Entry point of the application
+output_dir = "dist"  # Directory for the built executable
+icon_path = "img/icon.ico"  # Path to app icon (optional)
+customtkinter_path = os.path.join(os.path.dirname(__file__), "uivenv", "Lib", "site-packages", "customtkinter")  # Adjust if needed
+
+# PyInstaller build command
+PyInstaller.__main__.run([
+    entry_file,
+    f"--name={project_name}",
+    f"--distpath={output_dir}",
+    "--onedir",  # Create a folder for the executable and dependencies
+    "--windowed",  # Hide the console window
+    f"--add-data=img{os.pathsep}img",  # Include the assets directory
+    f"--add-data={customtkinter_path}{os.pathsep}customtkinter",  # Include the CustomTkinter directory
+    f"--icon={icon_path}",  # Add the application icon (optional)
+])
--- app/cmd/client/ui/chat.py
+++ app/cmd/client/ui/chat.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from dateutil import parser
 import threading
 import time
